b14d739885e73679a5fdeb647c11b94f2a965e23
Juliano Martins de Souza <JulianoMarthins@Yahoo.com.br>
2025-03-12 22:43:54 -0300

    URI2621
    Criação de pesquisa no banco de dados realizando consulta usando o SQL e JPQL

--- uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
+++ uri2621/src/main/java/com/devsuperior/uri2621/repositories/ProductRepository.java
@@ -1,9 +1,35 @@
 package com.devsuperior.uri2621.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
+import com.devsuperior.uri2621.dto.ProductMinDTO;
 import com.devsuperior.uri2621.entities.Product;
+import com.devsuperior.uri2621.projections.ProductMinProjection;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+
+    // Pesquisa no banco de dados usando o SQL
+    @Query(
+            nativeQuery = true,
+            value = "SELECT products.name " +
+                    "FROM products " +
+                    "INNER JOIN providers ON products.id_providers = providers.id " +
+                    "WHERE products.amount BETWEEN :min AND :max " +
+                    "AND providers.name LIKE CONCAT(:beginName, '%')"
+    )
+    List<ProductMinProjection> searchSQL(Integer min, Integer max, String beginName);
+
+
+    // Pesquisa no banco de dados usando o JPQL
+    @Query(
+            value = "SELECT new com.devsuperior.uri2621.dto.ProductMinDTO(obj.name) " +
+                    "FROM Product obj " +
+                    "WHERE obj.amount BETWEEN :min AND :max " +
+                    "AND obj.provider.name LIKE CONCAT(:beginName, '%')"
+    )
+    List<ProductMinDTO> searchJPQL(Integer min, Integer max, String beginName);
 
 }
